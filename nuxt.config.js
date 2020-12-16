64da2759a6b39f5ffb4090a5258fbf7b2db5baa0
Yuta Suzuki <yuta.suzuki.0206@gmail.com>
2020-12-16 11:54:33 +0900

    add api

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [{ src: '~/plugins/amplify.js', ssr: false }],
-  
+
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
@@ -33,6 +33,7 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/vuetify
+
     '@nuxtjs/vuetify',
   ],
 
