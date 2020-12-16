27a04ff49e246f0e97f7747dc6686c4b7417ea0c
Yuta Suzuki <yuta.suzuki.0206@gmail.com>
2020-12-16 13:51:14 +0900

    add graphql

--- pages/index.vue
+++ pages/index.vue
@@ -1,93 +1,62 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <h1>TodoApp</h1>
+    <v-text-field v-model="name" label="Name"></v-text-field>
+    <v-text-field v-model="description" label="Description"></v-text-field>
+    <v-btn @click="createTodo">Create</v-btn>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.name }} : {{ todo.description }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import { API } from 'aws-amplify'
+import { createTodo } from '~/src/graphql/mutations'
+import { listTodos } from '~/src/graphql/queries'
+import { onCreateTodo } from '~/src/graphql/subscriptions'
 
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
+  data() {
+    return {
+      name: '',
+      description: '',
+      todos: [],
+    }
+  },
+  created() {
+    this.getTodos()
+    this.subscribe()
+  },
+  methods: {
+    async createTodo() {
+      const { name, description } = this
+      if (!name || !description) return false
+      const todo = { name, description }
+      await API.graphql({
+        query: createTodo,
+        variables: { input: todo },
+      })
+      this.name = ''
+      this.description = ''
+    },
+    async getTodos() {
+      const todos = await API.graphql({
+        query: listTodos,
+      })
+      this.todos = todos.data.listTodos.items
+    },
+    subscribe() {
+      API.graphql({ query: onCreateTodo }).subscribe({
+        next: (eventData) => {
+          const todo = eventData.value.data.onCreateTodo
+          if (this.todos.some((item) => item.name === todo.name)) return // remove duplications
+          this.todos = [...this.todos, todo]
+        },
+      })
+    },
   },
 }
 </script>
